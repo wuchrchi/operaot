6f9ed01ac3cd27ac5f48acd740596a933e8cf891
wuchrchi <sq12348338@gmail.com>
2024-09-19 15:33:42 +0800

    change final

--- vite.config.js
+++ vite.config.js
@@ -5,23 +5,16 @@ import vuetify from 'vite-plugin-vuetify'
 const path = require('path')
 
 export default defineConfig({
-  base: '/operator', // 'operator' 是你的 GitHub repo 名稱
+  base: '/operator/',  // 這裡設置為你的 GitHub repo 名稱
   plugins: [
     vue(),
     vuetify({
       autoImport: true,
     }),
   ],
-  define: { 'process.env': {} },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-});
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/operator'
-    : '/'
-}
+})
